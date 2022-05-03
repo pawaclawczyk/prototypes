0a82a919900929e3cb9c9f91e1b3148797402807
Paweł Wacławczyk <p.a.waclawczyk@gmail.com>
2022-05-03 13:13:08 +0200

    dagster-with-spark: run job on a remote Spark, load and write data to MinIO

--- dagster-with-spark/src/pipelines/real_time_advertiser_auction.py
+++ dagster-with-spark/src/pipelines/real_time_advertiser_auction.py
@@ -2,15 +2,15 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, to_date
 
 
-def embed_and_ingest() -> None:
+def embed_and_ingest(src: str, dst: str) -> None:
     spark = SparkSession.builder.appName("embed and ingest").getOrCreate()
-    ingest(spark)
+    ingest(spark, src, dst)
 
 
-def ingest(spark: SparkSession) -> None:
-    df = spark.read.csv("data/source/real_time_advertiser_auction.csv", header=True, inferSchema=True,
+def ingest(spark: SparkSession, src: str, dst: str) -> None:
+    df = spark.read.csv(src, header=True, inferSchema=True,
                         timestampFormat="yyyy-MM-dd HH:mm:ss")
 
     df = df.withColumnRenamed("date", "timestamp").withColumn("date", to_date(col("timestamp"))).drop("timestamp")
 
-    df.write.parquet("data/ingested/real_time_advertiser_auction/", partitionBy="date", mode="overwrite")
+    df.write.parquet(dst, partitionBy="date", mode="overwrite")
--- dagster-with-spark/src/workflows/real_time_advertiser_auction.py
+++ dagster-with-spark/src/workflows/real_time_advertiser_auction.py
@@ -1,33 +1,45 @@
-from dagster import JobDefinition, OpExecutionContext, repository, job, op
+from dagster import JobDefinition, OpExecutionContext, repository, job, op, Permissive
 from dagster_pyspark import pyspark_resource
 
 from pipelines.real_time_advertiser_auction import ingest, embed_and_ingest
 
 
 def configure_spark_session(values: dict):
-    return {
-        "spark_conf": {
-            "spark": {
-                "master": values["master"],
-                "app": {
-                    "name": values["app"]
-                }
-            }
-        }
+    hadoop_aws = "org.apache.hadoop:hadoop-aws:3.3.1"
+    conf = {
+        "spark.app.name": values["app"],
+        "spark.master": values["master"],
     }
+    conf.update(values["extra"])
+    if "spark.jars.packages" in conf and conf["spark.jars.packages"]:
+        conf["spark.jars.packages"] += ";" + hadoop_aws
+    else:
+        conf["spark.jars.packages"] = hadoop_aws
+    return {"spark_conf": conf}
 
 
-spark_standalone = pyspark_resource.configured(configure_spark_session, config_schema={"master": str, "app": str})
+spark_standalone = pyspark_resource.configured(configure_spark_session, config_schema={
+    "app": str,
+    "master": str,
+    "extra": Permissive(),
+})
 
 
-@op
-def ingest_with_embedded_spark() -> None:
-    embed_and_ingest()
+@op(config_schema={"src": str, "dst": str})
+def ingest_with_embedded_spark(context: OpExecutionContext) -> None:
+    embed_and_ingest(
+        context.op_config["src"],
+        context.op_config["dst"],
+    )
 
 
-@op(required_resource_keys={"spark"})
+@op(required_resource_keys={"spark"}, config_schema={"src": str, "dst": str})
 def ingest_with_spark_resource(context: OpExecutionContext) -> None:
-    ingest(context.resources.spark.spark_session)
+    ingest(
+        context.resources.spark.spark_session,
+        context.op_config["src"],
+        context.op_config["dst"],
+    )
 
 
 @job(resource_defs={"spark": spark_standalone})
