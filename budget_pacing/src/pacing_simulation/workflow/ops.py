f185e46d5708a0fe5a4ad458b129e271fc33f2f3
Paweł Wacławczyk <p.a.waclawczyk@gmail.com>
2022-06-13 15:21:30 +0200

    [budget pacing] spent analysis

--- budget_pacing/src/pacing_simulation/workflow/ops.py
+++ budget_pacing/src/pacing_simulation/workflow/ops.py
@@ -3,16 +3,14 @@ import os.path
 
 import numpy as np
 import pandas as pd
-from dagster import op, OpExecutionContext, List, In, Nothing, job, repository, Out, Output, IOManager, InputContext, \
+from dagster import op, OpExecutionContext, job, repository, Out, Output, IOManager, InputContext, \
     OutputContext, io_manager, InitResourceContext
 
 from pacing_simulation.ad_server.simulation import Simulation
 from pacing_simulation.distributions import custom_distribution, traffic_distribution, uniform_distribution
-from pacing_simulation.load_save import save_base_distribution
-from scenario import Scenario
 from src.pacing_simulation.ad_server.ad_server import Campaign, AsapPacing, AdServer, second_price_auction, \
     ThrottledPacing
-from src.pacing_simulation.analysis import make_report, aggregate_case_events
+from src.pacing_simulation.analysis import aggregate_case_events
 
 
 class PandasCSVIOManager(IOManager):
@@ -22,6 +20,7 @@ class PandasCSVIOManager(IOManager):
 
     def handle_output(self, context: OutputContext, df: pd.DataFrame):
         path = os.path.join(self.base_dir, context.metadata["path"] + self.file_postfix)
+        os.makedirs(os.path.dirname(path), exist_ok=True)
         df.to_csv(path)
 
     def load_input(self, context: InputContext) -> pd.DataFrame:
@@ -29,9 +28,24 @@ class PandasCSVIOManager(IOManager):
         return pd.read_csv(path)
 
 
+class CampaignsIOManager(PandasCSVIOManager):
+    def handle_output(self, context: OutputContext, camps: list[Campaign]):
+        df = pd.DataFrame((c.to_dict() for c in camps)).set_index("campaign_id")
+        super().handle_output(context, df)
+
+    def load_input(self, context: InputContext) -> list[Campaign]:
+        df = super().load_input(context)
+        return [Campaign(c["campaign_id"], c["planned_budget"], bid_value=c["bid_value"]) for _, c in df.iterrows()]
+
+
 @io_manager(config_schema={"base_dir": str})
 def pandas_csv_io_manager(init_context: InitResourceContext) -> IOManager:
     return PandasCSVIOManager(init_context.resource_config["base_dir"])
+
+
+@io_manager(config_schema={"base_dir": str})
+def campaigns_io_manager(init_context: InitResourceContext) -> IOManager:
+    return CampaignsIOManager(init_context.resource_config["base_dir"])
 
 
 WINDOWS = (24 * 60)
@@ -45,7 +59,7 @@ BID_VALUE_OFFSET = 1_000
         "delivery": float,
     },
     out={
-        "camps": Out(),
+        "camps": Out(io_manager_key="campaigns_io_manager", metadata={"path": "input/campaigns"}),
         "dist": Out(),
         "traffic": Out(),
     }
@@ -68,7 +82,7 @@ def prepare_input_data(context: OpExecutionContext):
     return Output(camps, "camps"), dist, Output(traffic, "traffic")
 
 
-@op(out=Out(io_manager_key="pandas_io_manager", metadata={"path": "asap"}))
+@op(out=Out(io_manager_key="pandas_io_manager", metadata={"path": "result/asap"}))
 def run_asap_case(camps: list[Campaign], traffic: np.ndarray) -> pd.DataFrame:
     pacing = AsapPacing()
     process = AdServer(pacing, second_price_auction, camps)
@@ -76,7 +90,7 @@ def run_asap_case(camps: list[Campaign], traffic: np.ndarray) -> pd.DataFrame:
     return aggregate_case_events(sim.run())
 
 
-@op(out=Out(io_manager_key="pandas_io_manager", metadata={"path": "throttled"}))
+@op(out=Out(io_manager_key="pandas_io_manager", metadata={"path": "result/throttled"}))
 def run_throttled_case(dist: np.ndarray, camps: list[Campaign], traffic: np.ndarray) -> pd.DataFrame:
     pacing = ThrottledPacing(dist, (2 * 60))
     process = AdServer(pacing, second_price_auction, camps)
@@ -84,7 +98,7 @@ def run_throttled_case(dist: np.ndarray, camps: list[Campaign], traffic: np.ndar
     return aggregate_case_events(sim.run())
 
 
-@op(out=Out(io_manager_key="pandas_io_manager", metadata={"path": "throttled_wo_ff"}))
+@op(out=Out(io_manager_key="pandas_io_manager", metadata={"path": "result/throttled_wo_ff"}))
 def run_throttled_without_fast_finnish_case(dist: np.ndarray, camps: list[Campaign],
                                             traffic: np.ndarray) -> pd.DataFrame:
     pacing = ThrottledPacing(dist)
@@ -93,7 +107,7 @@ def run_throttled_without_fast_finnish_case(dist: np.ndarray, camps: list[Campai
     return aggregate_case_events(sim.run())
 
 
-@op(out=Out(io_manager_key="pandas_io_manager", metadata={"path": "throttled_uniform"}))
+@op(out=Out(io_manager_key="pandas_io_manager", metadata={"path": "result/throttled_uniform"}))
 def run_throttled_with_uniform_forecast(camps: list[Campaign], traffic: np.ndarray) -> pd.DataFrame:
     pacing = ThrottledPacing(uniform_distribution(WINDOWS))
     process = AdServer(pacing, second_price_auction, camps)
@@ -102,7 +116,8 @@ def run_throttled_with_uniform_forecast(camps: list[Campaign], traffic: np.ndarr
 
 
 @job(resource_defs={
-    "pandas_io_manager": pandas_csv_io_manager.configured({"base_dir": "data"})
+    "pandas_io_manager": pandas_csv_io_manager.configured({"base_dir": "data"}),
+    "campaigns_io_manager": campaigns_io_manager.configured({"base_dir": "data"})
 })
 def run_all():
     camps, dist, traffic = prepare_input_data()
@@ -115,51 +130,6 @@ def run_all():
 @repository
 def simulations():
     return [run_all]
-
-
-@op(config_schema={"windows": int, "output_dir": str})
-def make_custom_distribution(context: OpExecutionContext) -> np.ndarray:
-    dist = custom_distribution(context.op_config["windows"])
-    save_base_distribution(context.op_config["output_dir"], dist)
-    return dist
-
-
-@op(config_schema={"windows": int})
-def make_uniform_distribution(context: OpExecutionContext) -> np.ndarray:
-    return custom_distribution(context.op_config["windows"])
-
-
-@op(config_schema={"requests": int})
-def make_requests_distribution(context: OpExecutionContext, base_dist: np.ndarray) -> np.ndarray:
-    return Scenario.make_requests_distribution(base_dist, context.op_config["requests"])
-
-
-@op(config_schema={"campaigns": int, "requests": int, "budget_to_requests_rate": float, "output_dir": str})
-def make_campaigns(context: OpExecutionContext) -> List[Campaign]:
-    return Scenario.make_campaigns(
-        context.op_config["campaigns"],
-        context.op_config["requests"],
-        context.op_config["budget_to_requests_rate"],
-        context.op_config["output_dir"],
-    )
-
-
-@op(config_schema={"name": str, "output_dir": str})
-def run_asap_case(context: OpExecutionContext, reqs_dist: np.ndarray, campaigns: List[Campaign]):
-    Scenario.make_and_run_asap_case(context.op_config["name"], context.op_config["output_dir"], reqs_dist,
-                                    campaigns)
-
-
-@op(config_schema={"name": str, "output_dir": str, "fast_finnish": int})
-def run_throttled_case(context: OpExecutionContext, base_dist: np.ndarray, reqs_dist: np.ndarray,
-                       campaigns: List[Campaign]):
-    Scenario.make_and_run_throttled_case(context.op_config["name"], context.op_config["output_dir"],
-                                         context.op_config["fast_finnish"], base_dist, reqs_dist, campaigns)
-
-
-@op(config_schema={"scenario": str, "template": str, "output_dir": str}, ins={"wait_for": In(Nothing)})
-def make_analysis(context: OpExecutionContext):
-    make_report(context.op_config["scenario"], context.op_config["template"], context.op_config["output_dir"])
 
 
 if __name__ == "__main__":
